f7674d2746d5df56b427f62ee6c273578f7bc155
Kris Mackintosh <kris@spaceage.co.nz>
2021-02-18 16:29:53 +1300

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-karma-wrong-deps-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
